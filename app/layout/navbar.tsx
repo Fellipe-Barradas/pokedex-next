b7ddf0160179aa983480e724fb292181a8653ce4
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-06-19 11:32:13 -0300

    Criado header e footer

---
+++ app/layout/navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+export default function Navbar() {
+  return (
+    <div className='bg-red-500 p-2  flex justify-between items-center'>
+      <h1 className=' text-3xl font-pokemon bg-clip-text text-white mb-2'>POKEDEX</h1>
+      <Image src={"/pokebola.png"} alt='Meus pokemons' width={40} height={40} className='mr-3'/>
+    </div>
+  )
+}
